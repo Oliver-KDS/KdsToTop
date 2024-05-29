6083b639b0d95b8e29c94285242f0fdd9aac3288
Kurth digital solutions UG <info@kds-kassel.de>
2024-05-29 04:42:46 +0200

    Add files via upload

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'kdstotop-wizard-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:kdstotop/Resources/Public/Icons/Extension.svg',
+    ],
+];
